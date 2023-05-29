85224189eb6371d6f6f08b820ed4a7c4346b5fc8
1Antz1 <flamboyantz14@gmail.com>
2023-05-29 13:22:07 +0100

    feat: added form submit functionality

--- react-to-do-list/src/components/CustomForm.jsx
+++ react-to-do-list/src/components/CustomForm.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import { PlusIcon } from "@heroicons/react/24/solid";
 
-const handleFormSubmit = (e) => {
-  e.preventDefault();
-  console.log(e);
-};
-
-export const CustomForm = () => {
+export const CustomForm = ({ addTask }) => {
+  const [task, setTask] = useState("");
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addTask({
+      name: task,
+      checked: false,
+      id: Date.now(),
+    });
+    setTask("");
+  };
   return (
     <form className="todo" onSubmit={handleFormSubmit}>
       <div className="wrapper">
@@ -14,8 +20,8 @@ export const CustomForm = () => {
           type="text"
           id="task"
           className="input"
-          //   value={task}
-          //   onInput={(e) => setTask(e.target.value)}
+          value={task}
+          onInput={(e) => setTask(e.target.value)}
           required
           autoFocus
           maxLength={60}
